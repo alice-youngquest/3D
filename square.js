c50f2f794783e0ca813b274d905225c36ef96ac6
Alice Margaret Youngquest <alice_youngquest@Hotmail.com>
2017-05-02 20:38:33 +1200

    putting in meshFloor

--- square.js
+++ square.js
@@ -1,55 +1,67 @@
-var scene, camera, renderer, mesh;
+var scene, camera, renderer, mesh
+var meshFloor
 
-var keyboard = {};
+var keyboard = {}
+var player = { height:1.0 }
 
 function init () {
-  scene = new THREE.Scene();
-  camera = new THREE.PerspectiveCamera(90, 1280/720, 0.1, 1000);
+  scene = new THREE.Scene()
+  camera = new THREE.PerspectiveCamera(90, 1280/720, 0.1, 1000)
 
   mesh = new THREE.Mesh(
     new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial({color:0xFF3366, wireframe:false})
-    //0xff99999999999999999
-  );
+    //new THREE.SphereGeometry( 60, 24, 16 ),
+    // new THREE.CylinderGeometry( ... ),
+    new THREE.MeshBasicMaterial({color:0xFF3366, wireframe:true})
+    //transparent: true, opacity: 0.5
+  )
 
-  scene.add(mesh);
+  scene.add(mesh)
 
-  camera.position.set(0,0,2.5);
-  camera.lookAt(new THREE.Vector3(0,0,0));
+  meshFloor = new THREE.Mesh(
+    new THREE.PlaneGeometry(10,10),
+    new THREE.MeshBasicMaterial({color:0xffffff, wireframe:true})
+  )
 
-  renderer = new THREE.WebGLRenderer();
-  renderer.setSize(1280, 720);
-  document.body.appendChild(renderer.domElement);
+  
+  scene.add(meshFloor)
 
-  animate();
+  camera.position.set(0,player.height,-5)
+  camera.lookAt(new THREE.Vector3(0,player.height,0))
+
+  renderer = new THREE.WebGLRenderer()
+  renderer.setSize(1280, 720)
+  document.body.appendChild(renderer.domElement)
+
+  animate()
 }
 
 function animate () {
-  requestAnimationFrame(animate);
+  requestAnimationFrame(animate)
 
-  mesh.rotation.x += 0.01;
-  mesh.rotation.y += 0.02;
+  mesh.rotation.x += 0.01
+  mesh.rotation.y += 0.02
 
   if(keyboard[37]){ //left arrow key
-    camera.rotation.y -= Math.PI * 0.01;
+    camera.rotation.y -= Math.PI * 0.01
   }
   if(keyboard[39]){ //right arrow key
-    camera.rotation.y += Math.PI * 0.01;
+    camera.rotation.y += Math.PI * 0.01
   }
 
-  renderer.render(scene, camera);
+  renderer.render(scene, camera)
 }
 
 function keyDown (event) {
-  keyboard[event.keyCode] = true;
+  keyboard[event.keyCode] = true
 }
 
 function keyUp (event) {
-  keyboard[event.keyCode] = false;
+  keyboard[event.keyCode] = false
 }
 
-window.addEventListener('keydown', keyDown);
-window.addEventListener('keyup', keyUp);
+window.addEventListener('keydown', keyDown)
+window.addEventListener('keyup', keyUp)
 
 
-window.onload = init;
+window.onload = init
